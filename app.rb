ae28167cf58a6304a93b865267bfaa6185e01631
Marcus <marcus.kerr94@gmail.com>
2018-07-29 11:02:04 +0100

    First commit, Infrastructure setup

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+
+class Rps < Sinatra::Base
+  get '/' do
+    'Rock paper Scissors, Lets Go!'
+  end
+  
+  # Start the server if ruby file executed directly
+  run! if app_file == $0
+end
