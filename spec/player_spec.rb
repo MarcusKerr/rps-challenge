7c56d182670c9a471e0c743993f3d9da26f04f83
Marcus <marcus.kerr94@gmail.com>
2018-07-29 13:15:44 +0100

    Created player class, name attribute reader (GREEN)

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,10 @@
 require 'player'
 describe Player do
   subject(:waka) { Player.new('Waka') }
-  it 'returns the players name' do
-    expect(waka.name).to eq 'Waka'
+
+  describe '#name' do
+    it 'returns the players name' do
+      expect(waka.name).to eq 'Waka'
+    end
   end 
 end
