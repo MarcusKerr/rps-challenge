6529ddadeec8ef3446f0ad07cba2865e8ee07c79
Marcus <marcus.kerr94@gmail.com>
2018-07-29 19:04:08 +0100

    play page shows options (GREEN)

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter name' do
   scenario 'register and see name' do
     sign_in_and_play
-    expect(page).to have_content 'Waka, lets play Rock, Paper, Scissors!'
+    expect(page).to have_content 'Waka, lets play!'
   end 
 end
--- spec/features/options_spec.rb
+++ spec/features/options_spec.rb
@@ -2,5 +2,7 @@ feature 'game options' do
   scenario 'see the options rock paper and scissors' do
     sign_in_and_play
     expect(page).to have_content 'Rock'
+    expect(page).to have_content 'Paper'
+    expect(page).to have_content 'Scissors'
   end
 end
