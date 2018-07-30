c1cae817f10da72951174078e582d0b070ba19ed
Marcus <marcus.kerr94@gmail.com>
2018-07-30 19:19:38 +0100

    Unit test, retrieve player (GREEN)

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,11 @@
 require 'game'
 describe Game do
+  subject(:game) { described_class.new(player) }
+  let(:player)   { double :player }
+
+  describe '#player' do
+    it 'retrieves the player' do
+      expect(game.player).to eq player
+    end 
+  end
 end
