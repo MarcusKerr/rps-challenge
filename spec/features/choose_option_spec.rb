57c5538d43cd621e49e16db6b0900ed7e0419999
Marcus <marcus.kerr94@gmail.com>
2018-07-30 16:48:15 +0100

    Feature: computer randomly chooses move (RED)

--- spec/features/choose_option_spec.rb
+++ spec/features/choose_option_spec.rb
@@ -1,4 +1,17 @@
 feature 'choosing one option' do
+
+  scenario 'select rock' do
+    sign_in_and_play
+    click_button 'Rock'
+    expect(page).to have_content 'You chose Rock!'
+  end
+
+  scenario 'select paper' do
+    sign_in_and_play
+    click_button 'Paper'
+    expect(page).to have_content 'You chose Paper!'
+  end
+
   scenario 'select scissors' do
     sign_in_and_play
     click_button 'Scissors'
